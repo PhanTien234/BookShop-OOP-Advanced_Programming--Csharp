bead3d9679cb0cf52a7b12853e7d51ae42eb8873
PhanTien234 <minhtienp328@gmail.com>
2022-10-18 20:27:10 +0700

    Create class GoldenEditionBook inherit to Book

---
+++ BookShop/GoldenEditionBook.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop;
+
+public class GoldenEditionBook : Book
+{
+    public GoldenEditionBook(int id, string title, string author, decimal price, int quantity) :
+        base(id, title, author, price, quantity)
+    {
+    }
+    
+}

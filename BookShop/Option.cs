7604c43490dc494a1377424950aa81524b1457fd
PhanTien234 <minhtienp328@gmail.com>
2022-10-22 09:11:10 +0700

    add class Option

---
+++ BookShop/Option.cs
@@ -0,0 +1,18 @@
+﻿namespace BookShop;
+
+public class Option
+{
+    public const string INPUT_BOOK = "";
+    public const int INIT = 0;
+    public const int ADD_BOOK = 1;
+    public const int SHOW_ALL_BOOKS = 2;
+    public const int SHOW_BY_ID = 3;
+    public const int SHOW_BY_NAME = 4;
+    public const int REMOVE_BOOK = 5;
+    public const int UPDATE_BOOK = 6;
+    public const int EXIT = 10;
+
+    public const string ADD_GOLD_EDITION_BOOK = "a";
+    public const string ADD_NORMAL_BOOK = "b";
+    public const string EXIT_ENTER_BOOK = "v";
+}
